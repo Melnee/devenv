9d097c4df2d327689625d18afa22f8f748de6945
Melanie Neilan <melanie.neilan@gmail.com>
2026-05-20 23:26:47 -0700

    icons support!

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,43 +1,42 @@
 return {
   "nvim-tree/nvim-tree.lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     require("nvim-tree").setup({
       view = {
         width = 30,
         side = "left",
       },
-			filters = {
-				dotfiles = false,
-				git_ignored = false,
-			},
-			-- prevent nvim-tree from being the last window so last window doesn't become nil if it's closed
-			actions = {
-				open_file = {
-					quit_on_open = false,
-				},
-			},
-			-- prevent nvim-tree from getting confused when focus shifts to it
-			update_focused_file = {
-				enable = true,
-				update_root = false,
-			},
-			renderer = {
-				icons = {
-					show = {
-						file = false,
-						folder = false,
-						folder_arrow = true,
-						git = false,
-					},
-					glyphs = {
-						folder = {
-							arrow_closed = "▶",
-							arrow_open = "▼",
-						},
-					},
-				},
-			},
-		})
+      filters = {
+        dotfiles = false,
+        git_ignored = false,
+      },
+      actions = {
+        open_file = {
+          quit_on_open = false,
+        },
+      },
+      update_focused_file = {
+        enable = true,
+        update_root = false,
+      },
+      renderer = {
+        icons = {
+          show = {
+            file = true,
+            folder = true,
+            folder_arrow = true,
+            git = true,
+          },
+          glyphs = {
+            folder = {
+              arrow_closed = "▶",
+              arrow_open = "▼",
+            },
+          },
+        },
+      },
+    })
     vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<cr>")
   end
 }
