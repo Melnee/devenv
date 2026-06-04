a62fcddc4de5f2a05e59618d2c522e06f0aa5b63
Melanie Neilan <melanie.neilan@gmail.com>
2026-06-04 12:48:18 -0700

    make indentation work right with checkmate!

--- nvim/.config/nvim/lua/plugins/checkmate.lua
+++ nvim/.config/nvim/lua/plugins/checkmate.lua
@@ -1,10 +1,25 @@
 return {
   "bngarren/checkmate.nvim",
-  ft = "markdown", -- Lazy loads for Markdown files matching patterns in 'files'
+  ft = "markdown",
   opts = {
     files = {
       "*.todo.md",
-      "*.todo"
-    }, -- any .md file (instead of defaults)
+      "*.todo",
+    },
   },
+  config = function(_, opts)
+    require("checkmate").setup(opts)
+
+    vim.api.nvim_create_autocmd("BufEnter", {
+      pattern = { "*.todo.md", "*.todo" },
+      callback = function(ev)
+        vim.keymap.set("n", ">>", function()
+          vim.cmd("normal! >>")
+          vim.schedule(function()
+            vim.cmd("normal! $")
+          end)
+        end, { buffer = ev.buf, noremap = true })
+      end,
+    })
+  end,
 }
