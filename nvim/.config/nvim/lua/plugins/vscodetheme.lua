fe0017af381a9b636489de887080227bc513ce51
Melanie Neilan <melanie.neilan@gmail.com>
2026-06-05 20:30:54 -0700

    fix treesitter and fix rainbow delimiters

--- nvim/.config/nvim/lua/plugins/vscodetheme.lua
+++ nvim/.config/nvim/lua/plugins/vscodetheme.lua
@@ -13,7 +13,15 @@ return {
       callback = function()
         vim.api.nvim_set_hl(0, "StatusLine", { bg = "#4A1A4A", fg = "white" })
         vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "#1e1e1e", fg = "gray" })
-      end,
+        -- rainbow delimiters
+        vim.api.nvim_set_hl(0, "RainbowDelimiterRed",    { fg = "#ff6b6b" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterOrange", { fg = "#ffd93d" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterYellow",   { fg = "#6bcbff" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterGreen", { fg = "#ff9f43" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterBlue",  { fg = "#6bff8e" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterViolet", { fg = "#c56bff" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterCyan",   { fg = "#6bffee" })
+        end,
     })
   end,
 }
