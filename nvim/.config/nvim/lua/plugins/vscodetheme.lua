871b2e3716738e869450818d67f4f80d3052029d
Melanie Neilan <melanie.neilan@gmail.com>
2026-06-05 20:36:02 -0700

    desaturate rainbow delimiters

--- nvim/.config/nvim/lua/plugins/vscodetheme.lua
+++ nvim/.config/nvim/lua/plugins/vscodetheme.lua
@@ -14,14 +14,14 @@ return {
         vim.api.nvim_set_hl(0, "StatusLine", { bg = "#4A1A4A", fg = "white" })
         vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "#1e1e1e", fg = "gray" })
         -- rainbow delimiters
-        vim.api.nvim_set_hl(0, "RainbowDelimiterRed",    { fg = "#ff6b6b" })
-        vim.api.nvim_set_hl(0, "RainbowDelimiterOrange", { fg = "#ffd93d" })
-        vim.api.nvim_set_hl(0, "RainbowDelimiterYellow",   { fg = "#6bcbff" })
-        vim.api.nvim_set_hl(0, "RainbowDelimiterGreen", { fg = "#ff9f43" })
-        vim.api.nvim_set_hl(0, "RainbowDelimiterBlue",  { fg = "#6bff8e" })
-        vim.api.nvim_set_hl(0, "RainbowDelimiterViolet", { fg = "#c56bff" })
-        vim.api.nvim_set_hl(0, "RainbowDelimiterCyan",   { fg = "#6bffee" })
-        end,
+        vim.api.nvim_set_hl(0, "RainbowDelimiterRed",    { fg = "#cc6666" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterOrange", { fg = "#cc8855" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterYellow", { fg = "#ccbb55" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterGreen",  { fg = "#66bb77" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterBlue",   { fg = "#66aacc" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterViolet", { fg = "#aa66cc" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterCyan",   { fg = "#66bbbb" })
+      end,
     })
   end,
 }
